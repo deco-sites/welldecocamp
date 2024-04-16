376d3914ae215d37458f5d0dcb53775620c4de87
wellington3873 <wellingtonrufino@lelabs.com.br>
2024-04-16 16:32:00 -0300

    exerc 5

--- sections/HorizontalProductCard.tsx
+++ sections/HorizontalProductCard.tsx
@@ -34,7 +34,7 @@ const HorizontalProductCard = (props: Props) => {
 export function LoadingFallback() {
   return (
     <div>
-      <img src={product.image[0].url} class="skeleton" />
+      <div class="skeleton w-full h-[128px]" />
       <h1 class="bg-gray-500 w-[32px]">{product.name}</h1>
       <p><span class="loading loading-spinner" /></p>
 
@@ -59,9 +59,9 @@ export function ErrorFallback({ error }: { error?: Error }) {
   // Você pode exibir uma mensagem de erro, registrar o erro ou renderizar uma interface de substituição
   return (
     <div>
-      <img />
-      <h1>Ceará</h1>
-      <p>Castelão</p>
+      <img src="https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/04/Fortaleza-Castelao-e1696422421670.jpg?w=1220&h=674&crop=1"/>
+      <h1>Fortaleza</h1>
+      <p>O Estádio Governador Plácido Castelo, também conhecido como Arena Castelão, ou simplesmente Castelão, é um estádio de futebol brasileiro localizado em Fortaleza, Ceará, e inaugurado em 1973.</p>
       <a href="/cultura">
         para saber mais
       </a>
--- sections/PartialImageGallery.tsx
+++ sections/PartialImageGallery.tsx
@@ -2,27 +2,26 @@ import { usePartialSection } from "deco/hooks/usePartialSection.ts";
 import { ImageWidget } from "apps/admin/widgets.ts";
 
 export interface Props {
-    images: ImageWidget[]
-     /** @default 3 */
-    visibleImages: number
+  images: ImageWidget[];
+  /** @default 3 */
+  visibleImages: number;
 }
-
 
 const PartialImageGallery = (props: Props) => {
+  const visibleImages = [...props.images].splice(0, props.visibleImages);
 
-    const visibleImages = [...props.images].splice(0, props.visibleImages);
-
-    console.log(visibleImages, props.images, props.visibleImages)
-
-    return <div>
-        {visibleImages.map(item => <img src={item}/>)}
-        <button {...usePartialSection<Props>({
-          props: { visibleImages: 4 },
-        })}>+</button>
-        
-
+  return (
+    <div>
+      {visibleImages.map((item) => <img src={item} />)}
+      {visibleImages.length !== props.images.length && <button
+        {...usePartialSection<Props>({
+          props: { visibleImages: props.visibleImages + 1 },
+        })}
+      >
+        +
+      </button>}
     </div>
+  );
+};
 
-}
-
-export default PartialImageGallery
+export default PartialImageGallery;
